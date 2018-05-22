f3c739772388d4f7bfb703c9cecfc52a1f2dfce2
graeme <grae22@gmail.com>
2018-05-22 18:25:55 +0000

    Adding solution.

---
+++ ConsoleBsp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleBsp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
